2ee76b621b029d8e77ed54f804afccd46a4a87ec
andrmoe <mail@andreasmoe.com>
2025-02-23 18:54:20 +0100

    Agent environment with test

--- agent.py
+++ agent.py
@@ -1,15 +1,51 @@
-from typing import Callable, TypeVar
+from typing import Callable, TypeVar, Iterable, Generic
+import random
+import warnings
 
 S = TypeVar("S")  # Type for states
 A = TypeVar("A")  # Type for actions
 
-class Agent:
-    def __init__(self, state: S, transition: Callable[[S, A], S], policy: Callable[[S], A]):
+
+class Agent(Generic[S, A]):
+    def __init__(self, state: S, policy: Callable[[S], Iterable[tuple[float, A]]]):
         self.state = state
-        self.transition = transition
         self.policy = policy
 
-    def act(self) -> tuple[A, S]:
-        action = self.policy(self.state)
-        self.state = self.transition(self.state, action)
-        return action, self.state
+    def act(self) -> A:
+        action_distribution = self.policy(self.state)
+        probabilities, actions = zip(*action_distribution)
+        action = random.choices(actions, probabilities, k=1)[0]
+        return action
+
+    def observe(self, state: S):
+        """
+        The environment is fully observable. So every observation gives the entire environment state
+        :param state: The state of the environment
+        """
+        self.state = state
+
+class Environment(Generic[S, A]):
+    def __init__(self, state: S, transition_func: Callable[[S, A], S]):
+        self.state = state
+        self.transition_func = transition_func
+
+    def transition(self, action: A) -> S:
+        self.state = self.transition_func(self.state, action)
+        return self.state
+
+
+class AgentEnvironment(Generic[S, A]):
+    def __init__(self, agent: Agent[S, A], environment: Environment[S, A]):
+        if agent.state != environment.state:
+            warnings.warn(f"The state for the agent and environment are different. "
+                          f"{agent.state=}, {environment.state=}, "
+                          f"The agent's state was changed to the environment state", stacklevel=2)
+            agent.state = environment.state
+        self.agent = agent
+        self.environment = environment
+
+    def step(self) -> S:
+        action = self.agent.act()
+        new_state = self.environment.transition(action)
+        self.agent.observe(new_state)
+        return new_state
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from gridworld import GridWorld
 from torch import Tensor
+from agent import Agent, Environment, AgentEnvironment
 
 def test_gridworld():
     data = Tensor([[ 0, -1, -1],
@@ -26,3 +27,13 @@ def test_gridworld():
     assert world.agent_pos == (2, 2)
     assert world.move(world.east) == False
     assert world.agent_pos == (2, 2)
+
+
+def test_generic_agent_environment():
+    # S = A = bool
+    agent = Agent(False, lambda state: ((0, state), (1, not state)))
+    environment = Environment(False, lambda state, action: action)
+    agent_environment = AgentEnvironment(agent, environment)
+    assert agent.state == environment.state == False
+    next_state = agent_environment.step()
+    assert next_state == agent.state == environment.state == True
