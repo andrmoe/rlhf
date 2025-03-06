ffe02fb33733b96a2307dc6811250ad25f33a510
andrmoe <mail@andreasmoe.com>
2025-03-06 23:45:50 +0100

    Added reward model and displaying reward function

--- main.py
+++ main.py
@@ -39,24 +39,40 @@ def q_values_in_matrix(world: GridWorld, agent: QLearningAgent[tuple[int, int], 
     return matrix
 
 
-def visualise_gridworld(world: GridWorld, agent: QLearningAgent[tuple[int, int], int]):
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+def rewards_in_matrix(agent_environment: AgentEnvironment[tuple[int, int], int]) -> torch.Tensor:
+    matrix = torch.zeros_like(agent_environment.environment.world_data)
+    for x in range(matrix.shape[1]):
+        for y in range(matrix.shape[0]):
+            matrix[x, y] = agent_environment.reward_func((x, y))
+    return matrix
+
+
+def visualise_gridworld(agent_environment: AgentEnvironment[tuple[int, int], int]):
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(10, 5))
     agent_dot, = ax1.plot([], [], 'ro', markersize=20)
+    world = agent_environment.environment
     gridworld_mesh = ax1.pcolormesh(world.world_data)
     ax1.invert_yaxis()
     ax1.set_title("World")
     ax2.set_title("Expected Future Returns (Q)")
-    matrix = q_values_in_matrix(world, agent)
+    ax3.set_title("Reward Function")
+    matrix = q_values_in_matrix(world, agent_environment.agent)
     q_mesh = ax2.pcolormesh(matrix.numpy(), vmin=-10, vmax=10)
     ax2.invert_yaxis()
+    rewards = rewards_in_matrix(agent_environment)
+    reward_mesh = ax3.pcolormesh(rewards.numpy(), vmin=-3, vmax=3)
+    ax3.invert_yaxis()
+
 
     def update(frame):
         agent_dot.set_data([world.state[1] + 0.5], [world.state[0] + 0.5])
         matrix = q_values_in_matrix(world, agent)
         q_mesh.set_array(matrix.numpy().flatten())
-        return agent_dot, q_mesh
+        reward_mesh.set_array(rewards.numpy().flatten())
+        return agent_dot, q_mesh, reward_mesh
     ani = animation.FuncAnimation(fig, update, frames=range(2), interval=10)
     plt.colorbar(q_mesh, ax=ax2)
+    plt.colorbar(reward_mesh, ax=ax3)
     plt.show()
 
 
@@ -84,4 +100,4 @@ if __name__ == '__main__':
     agent_environment = AgentEnvironment(agent, small_maze, lambda s: reward_func(small_maze, s))
     t = threading.Thread(target=agent_loop, args=[agent_environment], daemon=True)
     t.start()
-    visualise_gridworld(small_maze, agent)
+    visualise_gridworld(agent_environment)
---
+++ reward_model.py
@@ -0,0 +1,38 @@
+from typing import TypeVar, Generic, Callable
+import numpy as np
+import torch
+from torch import tensor, Tensor, nn
+
+S = TypeVar('S')
+
+
+class RewardModel(Generic[S], nn.Module):
+    def __init__(self, ensemble: [nn.Module], encode: Callable[[S], Tensor]):
+        super().__init__()
+        self.ensemble = nn.ModuleList(ensemble)
+        self.encode = encode
+
+    def reward(self, state: S) -> Tensor:
+        encoded = self.encode(state)
+        rewards = torch.stack([reward_model(encoded) for reward_model in self.ensemble])
+        return torch.stack((torch.mean(rewards), torch.var(rewards)))
+
+    def loss(self, traj0: [S], traj1: [S], preference: Tensor) -> float:
+        exp_sum0 = np.exp(sum([self.reward(state) for state in traj0]))
+        exp_sum1 = np.exp(sum([self.reward(state) for state in traj1]))
+        denominator = exp_sum0 + exp_sum1
+        return - preference[0]*np.log(exp_sum0/denominator) - preference[1]*np.log(exp_sum1/denominator)
+
+
+class RewardTable(nn.Module):
+    def __init__(self, shape: tuple[int, int]):
+        super().__init__()
+        self.table = nn.Parameter(torch.randn(shape))
+
+    def forward(self, indices: Tensor) -> Tensor:
+        return self.table[indices]
+
+
+class GridWorldRewardModel(RewardModel[tuple[int, int]]):
+    def __init__(self, shape: tuple[int, int], ensemble_size: int):
+        super().__init__([RewardTable(shape) for _ in range(ensemble_size)], lambda s: tensor(s, dtype=torch.long))
